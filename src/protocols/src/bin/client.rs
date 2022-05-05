a6768456b6cf707c6ec6b914fd12cbd41dd991f2
Orestis Alpos <orestis.alpos@inf.unibe.ch>
2022-05-05 22:15:33 +0200

    State Manager

--- src/protocols/src/bin/client.rs
+++ src/protocols/src/bin/client.rs
@@ -1,26 +1,59 @@
-pub mod requests {
-    tonic::include_proto!("requests");
-}
+// pub mod requests {
+//     tonic::include_proto!("requests");
+// }
 
-use requests::threshold_protocol_client::ThresholdProtocolClient;
-use requests::{ThresholdDecryptionRequest, ThresholdDecryptionResponse};
+use cosmos_crypto::dl_schemes::ciphers::bz03::Bz03ThresholdCipher;
+use cosmos_crypto::dl_schemes::ciphers::sg02::Sg02ThresholdCipher;
+use cosmos_crypto::dl_schemes::dl_groups::bls12381::Bls12381;
+use cosmos_crypto::dl_schemes::dl_groups::dl_group::DlGroup;
+use cosmos_crypto::interface::{ThresholdCipher, ThresholdCipherParams, PrivateKey};
+use cosmos_crypto::rand::{RngAlgorithm, RNG};
+use protocols::requests::threshold_protocol_client::ThresholdProtocolClient;
+use protocols::requests::{ThresholdDecryptionRequest, ThresholdDecryptionResponse, self};
+use cosmos_crypto::interface::Ciphertext;
+use rand::prelude::SliceRandom;
+use rand::thread_rng;
+use tonic::Request;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = ThresholdProtocolClient::connect("http://[::1]:50051").await?;
 
-    let request = tonic::Request::new(ThresholdDecryptionRequest {
-        sn: 1,
-        algorithm: requests::ThresholdCipher::Sg02 as i32,
-        dl_group: requests::DlGroup::Bls12381 as i32,
-        threshold: 3,
-        ciphertext: Vec::new(),
-    });
-
+    const k: usize = 11; // threshold
+    const N: usize = 15; // total number of secret shares
+    let mut rng = RNG::new(RngAlgorithm::MarsagliaZaman);
+    let sk_sg02_bls12381 = Sg02ThresholdCipher::generate_keys(k, N, Bls12381::new(), &mut rng);
+    let sk_bz03_bls12381 = Bz03ThresholdCipher::generate_keys(k, N, Bls12381::new(), &mut rng);
+    
+    let request = create_decryption_request::<Sg02ThresholdCipher<Bls12381>>(1, &sk_sg02_bls12381[0].get_public_key());
     let response = client.decrypt(request).await?;
 
     println!("RESPONSE={:?}", response);
     
     Ok(())
+}
+
+fn create_decryption_request<C:ThresholdCipher>(sn: u32, pk: &C::TPubKey) -> tonic::Request<ThresholdDecryptionRequest> {
+    let mut params = ThresholdCipherParams::new();
+    let msg_string = format!("Test message {}", sn);
+    let msg: Vec<u8> = msg_string.as_bytes().to_vec();
+    let label = format!("Label {}", sn);
+    let ciphertext = C::encrypt(&msg, label.as_bytes(), pk, &mut params);
+    let req = requests::ThresholdDecryptionRequest {
+        algorithm: requests::ThresholdCipher::Sg02 as i32,
+        dl_group: requests::DlGroup::Bls12381 as i32,
+        ciphertext: ciphertext.get_msg(),
+    };
+    Request::new(req)
+}
+
+fn get_decryption_shares_permuted<C: ThresholdCipher>(K: usize, ctxt: &C::CT, sk: &Vec<C::TPrivKey>) -> Vec<C::TShare> {
+    let mut params = ThresholdCipherParams::new();
+    let mut shares = Vec::new();
+    for i in 0..K {
+        shares.push(C::partial_decrypt(ctxt,&sk[i as usize], &mut params));
+    }
+    shares.shuffle(&mut thread_rng());
+    shares
 }
--- src/protocols/src/bin/server.rs
+++ src/protocols/src/bin/server.rs
@@ -6,6 +6,7 @@ use cosmos_crypto::interface::{ThresholdCipherParams, Ciphertext, Serializable};
 use cosmos_crypto::rand::{RNG, RngAlgorithm};
 use rand::prelude::SliceRandom;
 use rand::thread_rng;
+use tokio::sync::mpsc::error::TryRecvError;
 use tonic::{transport::Server, Request, Response, Status};
 use std::convert::TryInto;
 use std::{collections::HashSet, thread, sync::mpsc};
@@ -13,23 +14,22 @@ use cosmos_crypto::{dl_schemes::{ciphers::{sg02::{Sg02PublicKey, Sg02PrivateKey,
 use protocols::threshold_cipher_protocol::ThresholdCipherProtocol;
 use std::collections::{self, HashMap};
 
-// struct MessageMultiplexor {
-//     net_to_prot: HashMap<String, tokio::sync::mpsc::Sender<Vec<u8>> >,
-//     prot_to_net: tokio::sync::mpsc::Receiver<Vec<u8>>,   
-// }
 
-enum Command {
-    AddChannel {
-        receiver_id: Vec<u8>,
+enum StateUpdateCommand {
+    AddNetToProtChannel {
+        instance_id: Vec<u8>,
         sender: tokio::sync::mpsc::Sender<Vec<u8>>
     },
-    SendMessage {
-        receiver_id: Vec<u8>,
+    AddProtToNetChannel {
+        instance_id: Vec<u8>,
+        receiver: tokio::sync::mpsc::Receiver<Vec<u8>>
+    },
+    ForwardMessageToProt {
+        instance_id: Vec<u8>,
         message: Vec<u8>,
     }
 }
 
-// #[derive(Debug)]
 pub struct Context {
     pub threshold: usize,
     pub pk_sg02_bls12381: Sg02PublicKey<Bls12381>,
@@ -39,26 +39,36 @@ pub struct Context {
 }
 
 /*
-The idea is that there exists a single RequestHandler, and on_receive_decrypt_request() is run every time a request is received.
-It checks it's exact type and starts the appropriate protocol in an new thread.
-todo: Give the rx of a channel to the thread so we can pass p2p/abc messages directly to the it and the tx of a channel so it can send messages to the p2p/abc layer. (?)
-todo: Do not pass the request to the protocol, but only the data it needs (request.threshold and request.ciphertext).
-This way the IF of the request will not be tied to the protocol but only to the RequestHandler.*/
+Request Handler:
+The idea is that there exists a single request handler struct (the ThresholdProtocolService),
+and the corresponding handler method is run every time a request is received.
+It checks the exact type of the request and starts the appropriate protocol as a new tokio task.
+Each protocol (tokio task) owns: 
+1) chan_in: the receiver end of a network-to-protocol channel, used for receiving messages (such as shares) from the network, and
+2) chan_out: the sender end of a protocol-to-network channel, used for sending messages to the network.
+These channels are created by the handler just before spawning the new tokio task.
+
+State Manager:
+There exists a seperate tokio task, the StateManager, responsible for the folloqing:
+1) Handling the state of the request handler,
+i.e., the sender ends of the network-to-protocol channels and the receiver ends of protocol-to-network channels.
+The StateManager is created once, in the tokio::main function. It exposes a sender channel end to the request handler,
+called state_manager_tx. All updates to the state (i.e., adding and removing network-to-protocol and protocol-to-network
+channels) take place by sending a StateUpdateCommand on state_manager_tx.
+2) When the Request Handler receives a share it uses the State Manager to forward it to the intended protocol instance
+through the appropriate network-to-protocol channel. Shares are received asynchronously, hence we do not give the Request
+Handler direct access to the network-to-protocol channels. Instead, the Request Hadler sends the share to the State Manager
+as a StateUpdateCommand and the State Manager sends it over the appropriate channel.
+3) It loops over all protocol-to-network channels and forwards the messages to the Network.
+*/
 
 // #[derive(Debug, Default)]
 pub struct ThresholdProtocolService {
     context: Context,
-    // Sender end of the channel shared between ThresholdProtocolService and Message Handler.
-    // Used to send updates to the Message Handler.
-    message_handler_tx: tokio::sync::mpsc::Sender<Command>, 
-    // Sender end of a channel between each protocol instance and the Message Handler.
-    // Used to send messages from each protocol instance to the network
-    // existing_seq_numbers: HashSet<usize>,
-    // // todo: Ciphertexts are here just because we cannot yet deserialize the ones in the request. To be removed.
+    state_manager_tx: tokio::sync::mpsc::Sender<StateUpdateCommand>, 
     // ctxt_sg02: Sg02Ciphertext<Bls12381>,
     // ctxt_bz03: Bz03Ciphertext<Bls12381>,
 }
-
 
 #[tonic::async_trait]
 impl ThresholdProtocol for ThresholdProtocolService {
@@ -74,19 +84,19 @@ impl ThresholdProtocol for ThresholdProtocolService {
         // let mut prot: ThresholdCipherProtocol<_>; 
         match (requests::ThresholdCipher::from_i32(decryption_request.algorithm).unwrap(), requests::DlGroup::from_i32(decryption_request.dl_group).unwrap()) {
             (requests::ThresholdCipher::Sg02, requests::DlGroup::Bls12381)  => {
-                let ciphertext = Sg02Ciphertext::decode(decryption_request.ciphertext).unwrap(); 
+                let ciphertext = Sg02Ciphertext::deserialize(decryption_request.ciphertext.clone()).unwrap(); 
 
-                // Create a channel for network to instance communication and one for instance to network communication.
+                // Create a channel for network-to-protocol communication and one for protocol-to-network communication.
                 let (net_to_prot_tx, net_to_prot_rx) = tokio::sync::mpsc::channel::<Vec<u8>>(32);
                 let (prot_to_net_tx, prot_to_net_rx) = tokio::sync::mpsc::channel::<Vec<u8>>(32);
                 
-                // Give the sender end of the net_to_prot channel and the receiver end of prot_to_net to the message handler...
-                let cmd = Command::AddNetToProtChannel { receiver_id: ciphertext.get_label() , sender:net_to_prot_tx };
-                let c = self.message_handler_tx.send(cmd).await;
-                let cmd = Command::AddProtToNetChannel { receiver_id: ciphertext.get_label() , receiver:prot_to_net_rx };
-                let c = self.message_handler_tx.send(cmd).await;
+                // Update the state. We need to keep the sender end of net_to_prot channel and the receiver end of prot_to_net. The state manager takes ownership.
+                let cmd = StateUpdateCommand::AddNetToProtChannel { instance_id: ciphertext.get_label() , sender:net_to_prot_tx };
+                let c = self.state_manager_tx.send(cmd).await;
+                let cmd = StateUpdateCommand::AddProtToNetChannel { instance_id: ciphertext.get_label() , receiver:prot_to_net_rx };
+                let c = self.state_manager_tx.send(cmd).await;
 
-                // ...and the reciever end to the protocol instance.
+                // The receiver end of net_to_prot channel and the sender end of prot_to_net are given to the protocol. The protocol takes ownership.
                 let mut prot = ThresholdCipherProtocol::<Sg02ThresholdCipher<Bls12381>>::new(
                     self.context.threshold, 
                     self.context.pk_sg02_bls12381.clone(), 
@@ -101,15 +111,20 @@ impl ThresholdProtocol for ThresholdProtocolService {
                 });
             },
             (requests::ThresholdCipher::Bz02, requests::DlGroup::Bls12381) => {
-                let ciphertext = Bz03Ciphertext::decode(decryption_request.ciphertext).unwrap(); 
-                let mut prot = ThresholdCipherProtocol::<Bz03ThresholdCipher<Bls12381>>::new(self.context.threshold, self.context.pk_bz03_bls12381.clone(), self.context.sk_bz03_bls12381.clone(), ciphertext);
-                thread::spawn( move || {
-                    prot.run();
-                });
+                // let ciphertext = Bz03Ciphertext::deserialize(decryption_request.ciphertext).unwrap(); 
+                // let mut prot = ThresholdCipherProtocol::<Bz03ThresholdCipher<Bls12381>>::new(
+                //     self.context.threshold, 
+                //     self.context.pk_bz03_bls12381.clone(), 
+                //     self.context.sk_bz03_bls12381.clone(), 
+                //     ciphertext,);
+                // thread::spawn( move || {
+                //     prot.run();
+                // });
             },
         };
         let reply = requests::ThresholdDecryptionResponse {
-            label: format!("Received request {}.", decryption_request.get_label()),
+            // label: format!("Received request {}.", decryption_request.get_label()).encode_to_vec(),
+            label: format!("Received request ").encode_to_vec(),
         };
 
         Ok(Response::new(reply))
@@ -118,40 +133,50 @@ impl ThresholdProtocol for ThresholdProtocolService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Spawn Message Handler
-    let (message_handler_tx, message_handler_rx) = tokio::sync::mpsc::channel::<Command>(32);
+    // Spawn the State Manager
+    let (state_manager_tx, mut state_manager_rx) = tokio::sync::mpsc::channel::<StateUpdateCommand>(32);
     tokio::spawn(async move {
-        let channels_net_to_prot: HashMap<Vec<u8>, tokio::sync::mpsc::Sender<Vec<u8>> > = HashMap::new();
-        let channels_prot_to_net: Vec<(Vec<u8>, tokio::sync::mpsc::Receiver<Vec<u8>)>; // Vector of pairs channel/instance_id
+        let mut channels_net_to_prot: HashMap<Vec<u8>, tokio::sync::mpsc::Sender<Vec<u8>> > = HashMap::new();
+        let mut channels_prot_to_net: HashMap<Vec<u8>, tokio::sync::mpsc::Receiver<Vec<u8>> > = HashMap::new();
         loop {
-            match message_handler_rx.try_recv() { // Check if a command was sent to us, either to add a new net_to_prot channel or to send a message through a net_to_prot channel
+            // Handle incoming commands (i.e., requests to modify the state).
+            match state_manager_rx.try_recv() { 
                 Ok(cmd) => {
                     match cmd {
-                        Command::AddChannel { receiver_id, sender } => {
-                            net_to_prot.insert(receiver_id, sender); // sender will be update if receiver_id already exists
+                        StateUpdateCommand::AddNetToProtChannel { instance_id, sender } => {
+                            channels_net_to_prot.insert(instance_id, sender); // todo: right now sender will be updated if instance_id already exists
                         },
-                        Command::SendMessage { receiver_id, message } => {
-                            if net_to_prot.contains_key(&receiver_id) { // todo: Handle case where channel does not exist
-                                let &sender = net_to_prot.get(&receiver_id).unwrap();
+                        StateUpdateCommand::AddProtToNetChannel { instance_id, receiver } => {
+                            channels_prot_to_net.insert(instance_id, receiver); // todo: right now receiver will be updated if instance_id already exists
+                        },
+                        StateUpdateCommand::ForwardMessageToProt { instance_id, message } => {
+                            if channels_net_to_prot.contains_key(&instance_id) { // todo: Handle case where channel does not exist
+                                let sender = channels_net_to_prot.get(&instance_id).unwrap();
                                 sender.send(message);
                             }
-                        }
+                        },
                     }
                 },
+                Err(TryRecvError::Disconnected) => {}, // sender end closed
                 Err(_) => {}
             };
-            //todo: check each channel in channels_prot_to_net
-            match prot_to_net_rx.try_recv() { // Check if any of the protocols sent a message on the prot_to_net channel.
-                Ok(message) => {
-                    // Broadcast this message to everyone.
-                },
-                Err(_) => {}
-            };
+            // Handle messages from protocol instances (by sending them through the network)
+            // todo: Move into a separate module?
+            for (instance, receiver) in channels_prot_to_net.iter_mut(){
+                match receiver.try_recv() {
+                    Ok(message) => {
+                        // Broadcast this message to everyone.
+                    },
+                    Err(TryRecvError::Disconnected) => {}, // sender end closed
+                    Err(_) => {}
+                };
+            }
+            
         }
     });
     
 
-    // Setup
+    // Setup the request handler
     // todo: Remove code, read keys, id, IP address from config files, once you can (de)serialize
     const k: usize = 3; // threshold
     const n: usize = 4; // total number of secret shares
@@ -171,8 +196,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
     let service = ThresholdProtocolService{
         context,
-        message_handler_tx,
-        // existing_seq_numbers : HashSet::new()
+        state_manager_tx,
     };
 
     Server::builder()
