fff3714c190ca083896c11c3960bb0dc5846efc7
nf20m547@campus.unibe.ch <nf20m547@campus.unibe.ch>
2022-05-18 13:26:49 +0900

    cleaned up folders

--- src/network/src/main.rs
+++ src/network/src/main.rs
@@ -1,73 +1,11 @@
-use libp2p::{identity, PeerId};
-use std::error::Error;
 
-// use cosmos_crypto::{interface::ThresholdCipher, dl_schemes::ciphers::sg02::{Sg02ThresholdCipher}, rand::RngAlgorithm};
-// use {cosmos_crypto::dl_schemes::{ciphers::sg02::{Sg02DecryptionShare}, dl_groups::{ bls12381::Bls12381}}};
+use cosmos_crypto::dl_schemes::ciphers::sg02::SG02_ThresholdCipher;
 
-// 1. create message
-// 2. get addresses from tendermint network
-// 3. broadcast message to all peers
-
-
-// use reqwest;
-// use std::error::Error;
-// use serde::Deserialize;
-
-
-// #[derive(Deserialize, Debug)]
-// struct Peer {
-//     node_id: i16,
-//     url: String,
-// }
-
-// #[derive(Deserialize, Debug)]
-// struct Response {
-//     listening: bool,
-//     listeners: Vec<String>,
-//     n_peers: i32,
-//     peers: Vec<Peer>,
-// }
-
-// use std::collections::HashMap;
-
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn Error>> {
-//     // let mut map = HashMap::new();
-//     // map.insert("lang", "rust");
-//     // map.insert("body", "json");
-
-//     let client = reqwest::Client::new();
-//     // let res = client.post("http://127.0.0.1:26657/net_info")
-//     //     .json(&map)
-//     //     .send()
-//     //     .await?;
-//     // println!("{:#?}", res);
-
-//     let response = client
-//         .post("http://127.0.0.1:26657/net_info")
-//         .header("ACCEPT", "application/json")
-//         .header("CONTENT_TYPE", "application/json")
-//         // .json(&map)
-//         .send()
-//         .await?
-//         // .unwrap();
-//         .text()
-//         .await?;
-//     println!("{:#?}", response);
-
-//     Ok(())
-// }
-
-
-// use cosmos_crypto::{interface::ThresholdCipher, dl_schemes::ciphers::sg02::{Sg02ThresholdCipher}, rand::RngAlgorithm};
-// cosmos_crypto::dl_schemes::{ciphers::sg02::{Sg02DecryptionShare}, dl_groups::{ bls12381::Bls12381}};
-// cosmos_crypto::interface;
+use crate::send::send::SendMessage;
+use crate::deliver::deliver::DeliverMessage;
 
 mod send;
-mod receive;
-use send::SendMessage;
-use receive::DeliverMessage;
-use cosmos_crypto::dl_schemes::ciphers::sg02::SG02_ThresholdCipher;
+mod deliver;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -80,7 +18,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     deliver_msg.deliver();
 
     // generate secret shares for SG02 scheme over Bls12381 curve
-    let sk = SG02_ThresholdCipher::generate_keys(K, N, Bls12381::new(), &mut rng);
+    // let sk = SG02_ThresholdCipher::generate_keys(K, N, Bls12381::new(), &mut rng);
     println!("Keys generated");
 
     // a public key is stored inside each secret share, so those can be used for encryption
