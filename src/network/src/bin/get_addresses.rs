8775dfc6213b366d458965cd617fa4ec10a9b3d5
nf20m547@campus.unibe.ch <nf20m547@campus.unibe.ch>
2022-05-19 17:35:49 +0900

    clean up code

--- src/network/src/bin/get_addresses.rs
+++ src/network/src/bin/get_addresses.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use cosmos_crypto::dl_schemes::ciphers::sg02::SG02_ThresholdCipher;
+use reqwest;
 
 // #[tokio::main]
 // pub async fn main() -> Result<(), Box<dyn Error>> {
@@ -8,34 +8,27 @@ use cosmos_crypto::dl_schemes::ciphers::sg02::SG02_ThresholdCipher;
 //     Ok(())
 // }
 
-// use reqwest;
-// use std::error::Error;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn Error>> {
-fn main() -> Result<(), Box<dyn Error>> {
-    // let mut map = HashMap::new();
-    // map.insert("lang", "rust");
-    // map.insert("body", "json");
+    let client = reqwest::Client::new();
+    let res = client.post("http://127.0.0.1:26657/net_info")
+        // .json(&map)
+        .send()
+        .await?;
+    println!("{:#?}", res);
 
-    // let client = reqwest::Client::new();
-    // let res = client.post("http://127.0.0.1:26657/net_info")
-    //     .json(&map)
-    //     .send()
-    //     .await?;
-    // println!("{:#?}", res);
-
-    // let response = client
-    //     .post("http://127.0.0.1:26657/net_info")
-    //     .header("ACCEPT", "application/json")
-    //     .header("CONTENT_TYPE", "application/json")
-    //     // .json(&map)
-    //     .send()
-    //     .await?
-    //     // .unwrap();
-    //     .text()
-    //     .await?;
-    // println!("{:#?}", response);
+    let response = client
+        .post("http://127.0.0.1:26657/net_info")
+        // .header("ACCEPT", "application/json")
+        // .header("CONTENT_TYPE", "application/json")
+        // .json(&map)
+        .send()
+        .await?
+        // .unwrap();
+        .text()
+        .await?;
+    println!("{:#?}", response);
 
     Ok(())
 }
--- src/network/src/bla.rs
+++ src/network/src/bla.rs
@@ -1,4 +1,4 @@
-use cosmos_crypto::dl_schemes::ciphers::sg02::SG02_ThresholdCipher;
+// use cosmos_crypto::dl_schemes::ciphers::sg02::SG02_ThresholdCipher;
 //use {cosmos_crypto::dl_schemes::{ciphers::sg02::{Sg02DecryptionShare}, dl_groups::{ bls12381::Bls12381}}};
 // use libp2p::{identity, PeerId};
 // use std::error::Error;
@@ -10,7 +10,7 @@ use cosmos_crypto::dl_schemes::ciphers::sg02::SG02_ThresholdCipher;
 // mod deliver;
 
 // #[async_std::main]
-fn main() {
+// fn main() {
     // let bc_msg = SendMessage { from: 123, msg: "hello world" };
     // bc_msg.broadcast();
     // let p2p_msg = SendMessage { from: 123, msg: "hello p2p world" };
@@ -26,4 +26,4 @@ fn main() {
     // // a public key is stored inside each secret share, so those can be used for encryption
     // let ciphertext = SG02_ThresholdCipher::encrypt(&msg, label, &sk[0].get_public_key(), &mut rng);
 
-}
+// }
