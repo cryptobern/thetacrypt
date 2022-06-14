45dafb6c770b0a3461081620b53bb9319d5c4a05
nf20m547@campus.unibe.ch <nf20m547@campus.unibe.ch>
2022-06-14 16:40:23 +0900

    code cleanup

--- src/network/src/bin/test_get_peers.rs
+++ src/network/src/bin/test_get_peers.rs
@@ -1,17 +1,4 @@
-use serde::{Deserialize, Serialize};
 use network::lib::get_peers;
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Response {
-    listening: bool,
-    peers: Vec<Peers>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Peers {
-    node_id: String,
-    url: String,
-}
 
 #[tokio::main]
 async fn main() {
@@ -19,10 +6,10 @@ async fn main() {
     match get_peers(test_addr.to_string()).await {
         Ok(peers) => {
             println!("{:#?}", peers);
-            // for peer in peers {
-            //     println!("node id: {:#?}", peer.node_id);
-            //     println!("node url: {:#?}", peer.url);
-            // }
+            for peer in peers {
+                println!("node id: {:#?}", peer.node_id);
+                println!("node url: {:#?}", peer.url);
+            }
         },
         Err(err) => println!("Error: {}", err),
     }
