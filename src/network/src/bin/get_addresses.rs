55cdd729f58be1a3deebdcccd7d2a90b353f83c3
nf20m547@campus.unibe.ch <nf20m547@campus.unibe.ch>
2022-06-14 11:15:31 +0900

    get tendermint node addresses with put request in nice format

--- src/network/src/bin/get_addresses.rs
+++ src/network/src/bin/get_addresses.rs
@@ -1,34 +1,26 @@
 use std::error::Error;
 use reqwest;
+use serde::{Deserialize, Serialize};
 
-// #[tokio::main]
-// pub async fn main() -> Result<(), Box<dyn Error>> {
-//     network::get_addresses();
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    listening: bool,
+    // listeners: Vec<Listener>,
+    peers: Vec<Peers>,
+}
 
-//     Ok(())
-// }
+#[derive(Serialize, Deserialize, Debug)]
+struct Peers {
+    node_id: String,
+    url: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 
-    let client = reqwest::Client::new();
-    let res = client.post("http://127.0.0.1:26657/net_info")
-        // .json(&map)
-        .send()
-        .await?;
-    println!("{:#?}", res);
-
-    let response = client
-        .post("http://127.0.0.1:26657/net_info")
-        // .header("ACCEPT", "application/json")
-        // .header("CONTENT_TYPE", "application/json")
-        // .json(&map)
-        .send()
-        .await?
-        // .unwrap();
-        .text()
-        .await?;
-    println!("{:#?}", response);
+    let http_response = reqwest::get("http://127.0.0.1:26657/net_info").await?;
+    let response = http_response.json::<Response>().await?;
+    println!("{:#?}", response.peers);
 
     Ok(())
 }
