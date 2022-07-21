dcd2570a4856dc3aeebb7255be5fa6b94724271b
nf20m547@campus.unibe.ch <nf20m547@campus.unibe.ch>
2022-07-21 09:12:45 +0900

    clean up test code

--- src/network/src/bin/test_gossipsub_setup.rs
+++ src/network/src/bin/test_gossipsub_setup.rs
@@ -30,21 +30,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // // sends a Vec<u8> into the channel as spawned thread
     tokio::spawn(async move {
-        // test vector
-        // let my_vec: Vec<u8> = [0b01001100u8, 0b11001100u8, 0b01101100u8].to_vec();
-        // let my_msg = P2pMessage { instance_id: 1.to_string(), message_data: my_vec };
-        // repeated sending in endless loop for testing purpose
+        // repeated sending in loop for testing purpose
         for count in 0..5 {
             // waits for sending the next message
             time::sleep(Duration::from_millis(10000)).await;
+
+            // test vector
             let mut my_vec: Vec<u8> = [0b01001100u8, 0b11001100u8, 0b01101100u8].to_vec();
-            
-            // my_vec[0] = count; // to keep track of the messages
+            my_vec[0] = count; // to keep track of the messages
             my_vec[1] = rand::random(); // to prevent dublicate messages
+            // test msg
             let my_msg = P2pMessage { instance_id: 1.to_string(), message_data: my_vec };
-            // sends Vec<u8> into the channel
-            // chn_out_send.send(my_vec.to_vec()).unwrap();
-            // my_msg = P2pMessage {instance_id: 1.to_string(), message_data: my_vec };
+            
+            // sends msg into the channel
             println!(">> TEST: SEND ->: {:?}", my_msg);
             match protocols_to_net_sender.send(my_msg).await {
                 Ok(_) => (),
--- src/network/src/bin/test_tendermint_setup.rs
+++ src/network/src/bin/test_tendermint_setup.rs
@@ -19,22 +19,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // // sends a Vec<u8> into the channel as spawned thread
     tokio::spawn(async move {
-        // test vector
-        // let my_vec: Vec<u8> = [0b01001100u8, 0b11001100u8, 0b01101100u8].to_vec();
-        // let my_msg = P2pMessage { instance_id: 1.to_string(), message_data: my_vec };
-        // repeated sending in endless loop for testing purpose
+        // repeated sending in loop for testing purpose
         for count in 0.. {
             // waits for sending the next message
             time::sleep(Duration::from_millis(20000)).await;
+
+            // test vector
             let mut my_vec: Vec<u8> = [0b01001100u8, 0b11001100u8, 0b01101100u8].to_vec();
-            
-            // my_vec[0] = count; // to keep track of the messages
+            my_vec[0] = count; // to keep track of the messages
             my_vec[1] = rand::random(); // to prevent dublicate messages
             let peer_id = tendermint_config::config_service::get_tendermint_node_id().await;
+            // test msg
             let my_msg = P2pMessage { instance_id: peer_id, message_data: my_vec };
-            // sends Vec<u8> into the channel
-            // chn_out_send.send(my_vec.to_vec()).unwrap();
-            // my_msg = P2pMessage {instance_id: 1.to_string(), message_data: my_vec };
+            
+            // sends msg into the channel
             println!(">> TEST: SEND ->: {:?}", my_msg);
             match protocols_to_net_sender.send(my_msg).await {
                 Ok(_) => (),
