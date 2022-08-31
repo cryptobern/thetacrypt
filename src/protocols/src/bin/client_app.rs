49cf6a49226719e80db8bd4e104bdc3caf8232f7
Mariarosaria Barbaraci <mariarosariabarbaraci@Mariarosarias-MacBook-Pro.local>
2022-08-31 12:07:06 +0200

    refactoring json serialization for abci_query

--- src/protocols/src/bin/client_app.rs
+++ src/protocols/src/bin/client_app.rs
@@ -27,12 +27,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                                                        "get_encryption_keys").await {
         Ok(res) => {
             // the key_id is returned in res.result.key and the actual key bytes in res.result.value.
-            match PublicKey::deserialize(&res.result.value) {
+            match PublicKey::deserialize(&res.result.response.value) {
                 Ok(pk) => {
-                    (String::from_utf8(res.result.key)?, pk)
+                    (String::from_utf8(res.result.response.key)?, pk)
                 },
                 Err(err) => {
-                    println!(">> The query either returned no key, or the key could not be deserialized.");
+                    println!(">> The query either returned no key, or the key could not be deserialized. Err {:?}", err);
                     return Ok(());
                 },
             }
@@ -60,22 +60,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 async fn query_tendermint_node(tendermint_node_ip: String,
                                tendermint_node_rpc_port: u16,
-                               tx: &str) -> Result<RPCResult<QueryResult>, Box<dyn Error>> {
+                               tx: &str) -> Result< RPCResult<RPCResponse<QueryResult>>, Box<dyn Error> > {
     println!(">> Start Query");
+
     let address = format!("http://{tendermint_node_ip}:{tendermint_node_rpc_port}");
     let req_url = address + "/abci_query?path=&data=" + tx;
+    
+    //De-comment for testing with POSTMAN mock server 
+    // let address = format!("https://25492e3f-d30c-499b-8b1a-75efcd8870eb.mock.pstmn.io");
+    // let req_url = address + "/abci_query?path=&data=" + tx + "&PMAK-630e38a93466ec3eeebad3dc-40895e607418ddcdbe2fa003b59878b367";
+
     println!(">> Url query: {}", req_url);
-    let response = reqwest::get(req_url).await?.json::<RPCResult<QueryResult>>().await?;
+    let response = reqwest::get(req_url).await?.json::<RPCResult<RPCResponse<QueryResult>>>().await?;
     println!(">> End Query");
     Ok(response)
 }
 
 async fn submit_tx_to_tendermint_node(tendermint_node_ip: String,
                                      tendermint_node_rpc_port: u16,
-                                     tx: Vec<u8>) -> Result<RPCResult<BroadcastTxResult>, Box<dyn Error>> {
+                                     tx: Vec<u8>) -> Result<RPCResult<BroadcastTxCommitResult>, Box<dyn Error>> {
     let address = format!("{tendermint_node_ip}:{tendermint_node_rpc_port}");
     let req_url = address + "/broadcast_tx_commit?tx=" + std::str::from_utf8(&tx)?;
-    let response = reqwest::get(req_url).await?.json::<RPCResult<BroadcastTxResult>>().await?;
+    let response = reqwest::get(req_url).await?.json::<RPCResult<BroadcastTxCommitResult>>().await?;
     Ok(response)
 }
 
@@ -94,6 +100,11 @@ pub struct RPCResult<R> {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct RPCResponse<R>{
+    pub response: R,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct BroadcastTxResult {
     pub code: i8,
     pub data: String,
@@ -102,6 +113,41 @@ pub struct BroadcastTxResult {
     pub hash: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BroadcastTxCommitResult {
+    pub check_tx: CheckTx,
+    pub deliver_tx: DeliverTx,
+    pub hash: String,
+    pub height: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CheckTx {
+    pub code: u32,
+    pub data: Option<String>,
+    pub log: String,
+    pub info: String,
+    pub gas_wanted: String,
+    pub gas_used: String,
+    pub events: Vec<String>,
+    pub codespace: String,
+    pub sender: String,
+    pub priority: String,
+    pub mempoolError: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DeliverTx {
+    pub code: u32,
+    pub data: Option<String>,
+    pub log: String,
+    pub info: String,
+    pub gas_wanted: String,
+    pub gas_used: String,
+    pub events: Vec<String>,
+    pub codespace: String,
+} 
+
 fn parse_proof<'de, D>(d: D) -> Result<String, D::Error> where D: Deserializer<'de> {
     Deserialize::deserialize(d)
         .map(|x: Option<_>| {
@@ -109,18 +155,20 @@ fn parse_proof<'de, D>(d: D) -> Result<String, D::Error> where D: Deserializer<'
         })
 }
 
-
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct QueryResult {
     pub code: u32,
     pub log: String,
     pub info: String,
-    pub index: i64,
-    pub key: Vec<u8>, 
+    pub index: String,
+    #[serde(with = "serde_bytes")]
+    pub key: Vec<u8>,
+    #[serde(with = "serde_bytes")]
     pub value: Vec<u8>,
-    #[serde(deserialize_with="parse_proof")]
-    pub proof_ops: String, //Segmentation fault could be because of the null here in the json?
-    pub height: i64,
+    //#[serde(deserialize_with="parse_proof")]
+    #[serde(alias="proofOps")]
+    pub proof_ops: Option<String>, 
+    pub height: String,
     pub codespace: String,
 }
 
