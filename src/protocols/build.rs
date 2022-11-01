0f1c18bda03c7630178ffd01309c2914962c8af8
Orestis Alpos <orestis.alpos@inf.unibe.ch>
2022-11-01 14:34:04 +0100

    upd build.rs

--- src/protocols/build.rs
+++ src/protocols/build.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
     .extern_path(".scheme_types", "::cosmos_crypto::proto::scheme_types")
     .out_dir("./src/proto")
-    .compile(&["./src/proto/protocol_types.proto"], &["./src/proto", "../src/proto"])?;
+    .protoc_arg("--experimental_allow_proto3_optional")
+    .compile(&["./src/proto/protocol_types.proto"], &["./src/proto", "../src/proto"])?; 
     Ok(())
 }
