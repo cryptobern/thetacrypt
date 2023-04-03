b9d216e08a2c1e1c26c961e604477f0523d2ae63
Mariarosaria Barbaraci <mariarosaria.barbaraci@inf.unibe.ch>
2023-04-03 11:49:22 +0200

    confgen modified for the integration

--- src/network/src/proxy/proxy.rs
+++ src/network/src/proxy/proxy.rs
@@ -1,93 +1,94 @@
-use std::error::Error;
-use std::io;
-use log::debug;
+// use std::error::Error;
+// use std::io;
+// use log::debug;
 
-// Tokio 
-use tokio::net::{TcpStream, TcpListener};
-use tokio::io::{AsyncWriteExt, Interest};
-use tokio::sync::mpsc::{Receiver, Sender};
+// // Tokio 
+// use tokio::net::{TcpStream, TcpListener};
+// use tokio::io::{AsyncWriteExt, Interest};
+// use tokio::sync::mpsc::{Receiver, Sender};
 
-// Thetacrypt
-use crate::config::static_net::config_service::*;
-use crate::types::message::P2pMessage;
+// // Thetacrypt
+// use crate::config::static_net::config_service::*;
+// use crate::types::message::P2pMessage;
 
-pub async fn init(
-    outgoing_msg_receiver: Receiver<P2pMessage>,
-    incoming_msg_sender: Sender<P2pMessage>,
-    config: Config, //create a config for this 
-    my_id: u32,
-) {
+// pub async fn init(
+//     outgoing_msg_receiver: Receiver<P2pMessage>,
+//     incoming_msg_sender: Sender<P2pMessage>,
+//     config: Config, //create a config for this 
+//     my_id: u32,
+// ) {
 
-    // load listener address from config file
-    let listen_addr = get_p2p_listen_addr(&config, my_id);
+//     // load listener address from config file
+//     let listen_addr = get_p2p_listen_addr(&config, my_id);
 
-    // Start proxy server
-    println!("[Connection-proxy] Start the server");
-    // Bind the listener to the address
-    let listener = TcpListener::bind(listen_addr).await.expect("Failed to bind the server");
-    println!("[Connection-proxy] Server started ...");
-        tokio::spawn(async move {
-            proxy_handler(listener).await
-        });
+//     // Start proxy server
+//     println!("[Connection-proxy] Start the server");
+//     // Bind the listener to the address
+//     let listener = TcpListener::bind(listen_addr).await.expect("Failed to bind the server");
+//     println!("[Connection-proxy] Server started ...");
+//         tokio::spawn(async move {
+//             proxy_handler(listener, incoming_msg_sender.clone()).await
+//         });
     
-    // Handle Channel Receiver
-    println!("[Connection-proxy] Start outgoing message forwarder");
-    tokio::spawn(async move {
-        outgoing_message_forwarder(outgoing_msg_receiver);
-    });
+//     // Handle Channel Receiver
+//     println!("[Connection-proxy] Start outgoing message forwarder");
+//     tokio::spawn(async move {
+//         outgoing_message_forwarder(outgoing_msg_receiver);
+//     });
     
-}
+// }
 
-pub async fn outgoing_message_forwarder(mut receiver: Receiver<P2pMessage>) -> io::Result<()> {
-    loop{
-        let msg = receiver.recv().await;
-        tokio::spawn(async move {
-            let Some(data) = msg else { return };
-            debug!("NET: Sending a message");
-            //here goes the tendermint ip
-            let stream = TcpStream::connect("localhost:8080").await.unwrap();
-            send_share(stream).await;
-        }); 
-    }
-}
+// pub async fn outgoing_message_forwarder(mut receiver: Receiver<P2pMessage>) -> io::Result<()> {
+//     loop{
+//         let msg = receiver.recv().await;
+//         tokio::spawn(async move {
+//             let Some(data) = msg else { return };
+//             debug!("NET: Sending a message");
+//             //here goes the tendermint ip
+//             let stream = TcpStream::connect("localhost:8080").await.unwrap();
+//             send_share(stream).await;
+//         }); 
+//     }
+// }
 
-pub async fn proxy_handler(listener: TcpListener, sender: Sender<P2pMessage>) -> io::Result<()> {
-    loop {
-        // The second item contains the IP and port of the new connection.
-        let (socket, _) = listener.accept().await.unwrap();
-        tokio::spawn(async move {
-            receive_share(socket, &sender).await;    //multiple threads need to read from here
-        });
-    }
-}
+// pub async fn proxy_handler(listener: TcpListener, sender: Sender<P2pMessage>) -> io::Result<()> {
+//     loop {
+//         // The second item contains the IP and port of the new connection.
+//         let (socket, _) = listener.accept().await.unwrap();
+//         let sender_cloned = sender.clone();
+//         tokio::spawn(async move {
+//             receive_share(socket, sender_cloned).await;    //multiple threads need to read from here
+//         });
+//     }
+// }
 
-pub async fn send_share(mut connection: TcpStream) -> io::Result<()> {
-    println!("[Connection-proxy Sender] Successfully connected to server in port 8080");
+// pub async fn send_share(mut connection: TcpStream) -> io::Result<()> {
+//     println!("[Connection-proxy Sender] Successfully connected to server in port 8080");
 
-    let msg = b"Hello!";
+//     let msg = b"Hello!";
 
-    connection.write_all(msg).await?;
-    println!("[connection-proxy Sender] Sent the share...");
+//     connection.write_all(msg).await?;
+//     println!("[connection-proxy Sender] Sent the share...");
 
-    Ok(())
-}
+//     Ok(())
+// }
 
-async fn receive_share(connection: TcpStream, sender: &Sender<P2pMessage>) -> io::Result<()> {
+// async fn receive_share(connection: TcpStream, sender: Sender<P2pMessage>) -> io::Result<()> {
 
-    let mut buffer = [0; 4096];
-    let ready = connection.ready(tokio::io::Interest::READABLE).await?;
+//     let mut buffer = [0; 4096];
+//     let ready = connection.ready(tokio::io::Interest::READABLE).await?;
     
-    if ready.is_readable(){
-        connection.try_read(&mut buffer);
-    }
+//     if ready.is_readable(){
+//         connection.try_read(&mut buffer);
+//     }
 
-    println!("[Connection-proxy Receiver] Received msg") ;
+//     println!("[Connection-proxy Receiver] Received msg") ;
 
-    //Send on the channel 
-    match sender.send(P2pMessage::from(buffer.to_vec())).await {
-        Ok(_) => (), 
-        Err(e) => println!(">> TEST: error send to network {e}"),
-    }
+//     //Send on the channel 
+//     match sender.send(P2pMessage::from(buffer.to_vec())).await {
+//         Ok(_) => (), 
+//         Err(e) => println!(">> TEST: error send to network {e}"),
+//     }
 
-    Ok(())
-}
+//     Ok(())
+// }
--- src/protocols/src/server/types.rs
+++ src/protocols/src/server/types.rs
@@ -15,6 +15,74 @@ pub struct Peer {
     pub rpc_port: u16,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ProxyNode {
+    pub ip: String,
+}
+
+/// Configuration of the server binary.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ServerProxyConfig {
+    /// ID of this server.
+    pub id: u32,
+    /// Address which the server will attempt to bind to.
+    pub listen_address: String,
+    pub p2p_port: u16,
+    pub rpc_port: u16,
+    /// Proxy node for forwarding the messages into a blockchian platform
+    pub proxy_node: ProxyNode,
+}
+
+impl ServerProxyConfig {
+
+    /// Read a server's configuration from a JSON encoding on disk.
+    pub fn from_file(file: &PathBuf) -> Result<ServerProxyConfig, String> {
+        let data = match fs::read_to_string(file) {
+            Ok(s) => s,
+            Err(e) => return Err(format!("Error reading config file: {}", e)),
+        };
+
+        match ServerProxyConfig::from_json(&data) {
+            Ok(cfg) => return Ok(cfg),
+            Err(e) => return Err(format!("Error parsing config file: {}", e)),
+        }
+    }
+
+    /// Build a server's configuration based on a JSON serialization.
+    pub fn from_json(data: &str) -> Result<ServerProxyConfig, String> {
+        let cfg: ServerProxyConfig = match serde_json::from_str(data) {
+            Ok(cfg) => cfg,
+            Err(e) => return Err(format!("Invalid JSON: {}", e)),
+        };
+
+        ServerProxyConfig::new(cfg.id, cfg.listen_address, cfg.p2p_port, cfg.rpc_port, cfg.proxy_node)
+    }
+
+    /// Initialize a new config struct. Performs a sanity check of passed values.
+    pub fn new(id: u32, listen_address: String, p2p_port: u16, rpc_port: u16, proxy_node: ProxyNode) -> Result<ServerProxyConfig, String> {
+        match IpAddr::from_str(&listen_address) {
+            Ok(_) => {}
+            Err(e) => return Err(format!("Invalid value for LISTEN_ADDRESS: {}", e)),
+        }
+
+        
+        match IpAddr::from_str(&proxy_node.ip) {
+            Ok(_) => {}
+            Err(e) => return Err(format!("Invalid IP for peer {}: {}", id, e)),
+        }
+        
+
+        Ok(ServerProxyConfig {
+            id,
+            listen_address,
+            p2p_port,
+            rpc_port,
+            proxy_node,
+        })
+    }
+
+}
+
 /// Configuration of the server binary.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ServerConfig {
