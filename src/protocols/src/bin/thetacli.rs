a2a9c6969755b0c94a8abd01e8661e6e8085bdb9
noahschmid <noah.schmid77@gmail.com>
2023-09-26 16:00:45 +0200

    Use clap for parsing cli arguments

--- src/protocols/src/bin/thetacli.rs
+++ src/protocols/src/bin/thetacli.rs
@@ -7,102 +7,124 @@ use protocols::keychain::KeyChain;
 use thetacrypt_proto::scheme_types::{ThresholdScheme, Group};
 use std::fs;
 
-fn main() -> Result<(), ()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        print_info();
-        return Err(());
-    }
+use clap::{Parser, ValueEnum, Subcommand, Args};
 
-    let action: String = args[1].parse().expect("The first argument should be the intended action");
-
-    if action.eq("keygen") {
-        if args.len() < 6 {
-            print_info();
-            return Err(());
-        }
-
-        let k: usize = args[2].parse().expect("The first argument should be an integer, the threshold.");
-        let n: usize = args[3].parse().expect("The second argument should be an integer, the number of parties.");
-        let a: String = args[4].parse().expect("The third argument should be a string, the algorithms and groups to generate keys for.");
-        let dir: String = args[5].parse().expect("The last argument should define the directory to store the keys in");
-        keygen(k, n, &a, &dir);
-    } else if action.eq("enc") {
-        if args.len() < 5 {
-            print_info();
-            return Err(());
-        }
-
-        let key_path: String = args[2].parse().expect("The first argument should be the path to the key file");
-        let infile: String = args[3].parse().expect("The second argument should be the path to the input file");
-        let label: String = args[4].parse().expect("The third argument should be the encryption label");
-        let outfile: String = args[5].parse().expect("The last argument should be the output path");
-
-        if let Ok(e) = encrypt(&infile, label.as_bytes(), &outfile, &key_path) {
-            println!("Successfully encrypted file.");
-            return Ok(());
-        }
-
-        return Err(());
-    } else if action.eq("verify") {
-        if args.len() < 5 {
-            print_info();
-            return Err(());
-        }
-
-        let key_path: String = args[2].parse().expect("The first argument should be the path to the key file");
-        let msgfile: String = args[3].parse().expect("The second argument should be the path to the message file");
-        let signature_file: String = args[4].parse().expect("The last argument should be the path to the signature file");
-
-        if let Ok(e) = verify(&key_path, &msgfile, &signature_file) {
-            return Ok(());
-        }
-
-        return Err(());
-    } else { 
-        print_info();
-        return Err(());
-    }
-
-    Ok(())
-
+#[derive(Parser, Debug)]
+#[command(version = "1.0", about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct ThetaCliArgs {
+    #[command(subcommand)]
+    command: Commands,
 }
 
-fn print_info() {
-    println!("usage: ./thetacli <action> <params>");
-    println!("available actions:");
-    println!("- keygen <k> <n> <algorithms> <directory>");
-    println!("\t generates the public/private keys for the specified schemes and groups");
-    println!("\t k = threshold");
-    println!("\t n = number of private keys");
-    println!("\t directory = directory to store generated keys in");
-    println!("\t algorithms = a list of comma separated elements of the format 'scheme-group', where 'scheme' is one of the following:");
-    println!("\t\t encryption schemes: sg02, bz03");
-    println!("\t\t signature schemes: bls04, frost, sh00");
-    println!("\t\t coin schemes: cks05");
-    println!("\t and 'group' is one of");
-    println!("\t\t 'bls12381', 'bn254', 'ed25519', 'rsa512', 'rsa1024', 'rsa2048'.");
-    println!("\t example: ./thetacli keygen 3 5 sg02-bls12381,bz03-ed25519 /path/to/keys/\n");
-    println!("- enc <pubkey> <infile> <label> <outfile>");
-    println!("\t encrypt a given infile and store it as outfile");
-    println!("\t pubkey = public key of a threshold encryption scheme");
-    println!("\t infile = file to be encrypted");
-    println!("\t label = label for ciphertext");
-    println!("\t outfile = file to store the encoded ciphertext in");
-    println!("- verify <pubkey> <msg> <signature>");
-    println!("\t pubkey = public key of a threshold encryption scheme");
-    println!("\t msg = signed message (bytes)");
-    println!("\t signature = signature to verify (hex encoded)");
+#[derive(Subcommand, Debug)]
+enum Commands {
+    keygen(KeyGenArgs),
+    enc(EncArgs),
+    verify(VerifyArgs)
 }
 
-fn keygen(k: usize, n: usize, a: &str, dir: &str) {
+#[derive(Args, Debug)]
+struct KeyGenArgs {
+    #[arg(short, 
+        help = "Threshold (minimum number of parties that need to collaborate)", 
+    )]
+    pub k: u16,
+    #[arg(
+        short,
+        help = "Number of parties"
+    )]
+    pub n: u16,
+    #[arg(
+        short,
+        long,
+        help = "A list of comma separated elements of the format 'scheme-group', where 'scheme' is one of the following:\n\t encryption schemes: sg02, bz03\n\t signature schemes: bls04, frost, sh00\n\t coin schemes: cks05\nand 'group' is one of\n\t 'bls12381', 'bn254', 'ed25519', 'rsa512', 'rsa1024', 'rsa2048'.\nexample: sg02-bls12381,bz03-ed25519",
+    )]
+    pub subjects: String,
+    #[arg(
+        short,
+        long,
+        help = "Directory to store the generated keys in",
+    )]
+    pub dir: String,
+}
+#[derive(Args, Debug)]
+struct EncArgs {
+    #[arg(
+        short, 
+        long,
+        help = "The path to the input file"
+    )]
+    pub infile: String,
+    #[arg(
+        short,
+        long,
+        help = "The path to the key file",
+    )]
+    pub key_path: String,
+    #[arg(
+        short,
+        long,
+        help = "The encryption label"
+    )]
+    pub label: String,
+    #[arg(
+        short,
+        long,
+        help = "The output path"
+    )]
+    pub outfile: String,
+}
+
+#[derive(Args, Debug)]
+struct VerifyArgs {
+    #[arg(
+        short,
+        long,
+        help = "The path to the public key file"
+    )]
+    pub key_path: String,
+    #[arg(
+        short,
+        long,
+        help = "The path to the file containing the message"
+    )]
+    pub message_path: String,
+    #[arg(
+        short,
+        long,
+        help = "The path to the file containing the signature",
+    )]
+    pub signature_path: String,
+}
+
+
+fn main() -> Result<(), ThresholdCryptoError> {
+    let args = ThetaCliArgs::parse();
+
+    if let Commands::keygen(keyGenArgs) = args.command {
+        return keygen(keyGenArgs.k, keyGenArgs.n, &keyGenArgs.subjects, &keyGenArgs.dir);
+    }
+
+    if let Commands::enc(encArgs) = args.command {
+        return encrypt(&encArgs.infile, encArgs.label.as_bytes(), &encArgs.outfile, &encArgs.key_path);  
+    }
+
+    if let Commands::verify(verifyArgs) = args.command {
+        return verify(&verifyArgs.key_path, &verifyArgs.message_path, &verifyArgs.signature_path);
+    }
+
+    return Ok(());
+}
+
+fn keygen(k: u16, n: u16, a: &str, dir: &str) -> Result<(), ThresholdCryptoError> {
     let parts = a.split(',');
     let mut keys = Vec::new();
     let mut rng = RNG::new(RngAlgorithm::OsRng);
 
     if fs::create_dir_all(dir).is_err() {
         println!("Error: could not create directory");
-        exit(-1);
+        return Err(ThresholdCryptoError::IOError);
     }
 
     for part in parts {
@@ -110,38 +132,38 @@ fn keygen(k: usize, n: usize, a: &str, dir: &str) {
 
         let scheme_str = s.next();
         if scheme_str.is_none() {
-            println!("Invalid format of argument 'algorithms'");
-            exit(-1);
+            println!("Invalid format of argument 'subjects'");
+            return Err(ThresholdCryptoError::InvalidParams);
         }
 
         let scheme = ThresholdScheme::parse_string(scheme_str.unwrap());
         if scheme.is_err() {
             println!("Invalid scheme '{}' selected", scheme_str.unwrap());
-            exit(-1);
+            return Err(ThresholdCryptoError::InvalidParams);
         }
 
         let group_str = s.next();
         if group_str.is_none() {
-            println!("Invalid format of argument 'algorithms'");
-            exit(-1);
+            println!("Invalid format of argument 'subjects'");
+            return Err(ThresholdCryptoError::InvalidParams);
         }
 
         let group = Group::from_str_name(group_str.unwrap());
         if group.is_none() {
             println!("Invalid group '{}' selected", group_str.unwrap());
-            exit(-1);
+            return Err(ThresholdCryptoError::InvalidParams);
         }
 
         let mut name = String::from(group_str.unwrap());
         name.insert_str(0, "_");
         name.insert_str(0, scheme_str.unwrap());
-        let key = KeyGenerator::generate_keys(k, n, &mut rng, &scheme.unwrap(), &group.unwrap(), &Option::None).expect("Failed to generate keys");
+        let key = KeyGenerator::generate_keys(k as usize, n as usize, &mut rng, &scheme.unwrap(), &group.unwrap(), &Option::None).expect("Failed to generate keys");
 
         let pubkey = key[0].get_public_key().serialize().unwrap();
         let file = File::create(format!("{}/{}.pub", dir, part));
         if let Err(e) = file.unwrap().write_all(&pubkey) {
             println!("Error storing public key: {}", e.to_string());
-            return;
+            return Err(ThresholdCryptoError::IOError);
         }
 
         keys.insert(0, (name, key));
@@ -150,7 +172,7 @@ fn keygen(k: usize, n: usize, a: &str, dir: &str) {
     for node_id in 0..n {
         let mut key_chain = KeyChain::new();
         for k in &keys {
-            key_chain.insert_key(k.1[node_id].clone(), k.0.clone()).expect("error generating key");
+            key_chain.insert_key(k.1[node_id as usize].clone(), k.0.clone()).expect("error generating key");
         }
 
         let keyfile = format!("{}/keys_{:?}.json", dir, node_id);
@@ -158,6 +180,7 @@ fn keygen(k: usize, n: usize, a: &str, dir: &str) {
     }
 
     println!("Keys successfully generated.");
+    return Ok(());
 }
 
 fn encrypt(infile: &str, label: &[u8], outfile: &str, key_path: &str) -> Result<(), ThresholdCryptoError> {
